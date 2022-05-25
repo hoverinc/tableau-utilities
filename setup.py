b30d56322431db5ea36f32b9232b106891048702
Justin Grilli <37851359+JustinGrilli@users.noreply.github.com>
2022-05-25 12:50:32 -0700

    Update setup (#2)
    * Added license to setup.py
    * added pypi url to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open('README.md') as f:
     readme = f.read()
@@ -7,11 +7,13 @@ setup(
     url="https://github.com/hoverinc/tableau-utilities",
     author="Justin Grilli",
     author_email="jgrilli@hover.to",
+    license='MIT',
+    url='http://pypi.python.org/pypi/tableau-utilities/',
     description='Utility for maintaining Tableau objects',
     long_description=readme,
     name="tableau_utilities",
     version="1.0.0",
-    packages=find_packages(),
+    packages=['tableau_utilities'],
     package_data={"tableau_utilities": ["item_types.yml"]},
     include_package_data=True,
     entry_points={
