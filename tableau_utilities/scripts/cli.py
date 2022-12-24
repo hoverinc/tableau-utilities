5279db90a039487543696b3dd033b0ee9274058f
Jay Rosenthal <jay.rosenthal@panther.io>
2022-12-24 06:02:06 -0800

    running the top level

--- tableau_utilities/scripts/cli.py
+++ tableau_utilities/scripts/cli.py
@@ -5,9 +5,7 @@ import shutil
 import sys
 from pprint import pprint
 
-from tableau_utilities.scripts.datasources_column_config_generate import main
-
-
+# from tableau_utilities.scripts.datasources_column_config_generate import main
 
 def do_args():
     """ Parse arguments.
@@ -16,24 +14,33 @@ def do_args():
     """
 
     parser = argparse.ArgumentParser(description='Tableau utilities clic')
-    parser.add_argument('-g', '--config_generate', action='store_true',
-                        help='CLI to generate column configs to automated editing of Tableau medatadata')
-    parser.add_argument('-m', '--conffig_merge',
-                        help='The path to the configuration. This code ASSUMES that the additional config is for a single datasource ')
-    parser.add_argument('-s', '--server_info',
-                        help='The name of the merged config JSON file.  For my_config.json enter my_config. Do not enter the .json extension')
-    parser.add_argument('-m', '--folder_name',
-                        help='Specifies the folder to write the datasource and configs to')
+    parser.add_argument('scriptname')
+
+    # parser.add_argument('-g', '--config_generate', action='store_true',
+    #                     help='CLI to generate column configs to automated editing of Tableau medatadata')
+    # parser.add_argument('-m', '--conffig_merge',
+    # parser.add_argument('-s', '--server_info',
+    #                     help='The name of the merged config JSON file.  For my_config.json enter my_config. Do not enter the .json extension')
+    # parser.add_argument('-m', '--folder_name',
+    #                     help='Specifies the folder to write the datasource and configs to')
 
     return parser.parse_args()
 
 
+def main():
+    args = do_args()
+    print("i made it to this version")
+    print(args.scriptname)
+
 
 if __name__ == '__main__':
-    args = do_args()
+    main()
 
-    if args.config_generate:
-        datasources_column_config_generate.main()
+
+
+
+    # if args.config_generate:
+    #     datasources_column_config_generate.main()
 
 
 
