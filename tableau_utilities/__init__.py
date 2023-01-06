902e2c57d866705b60ae0181e31f413c20756b12
Jay Rosenthal <jay.rosenthal@panther.io>
2023-01-06 04:44:45 -0800

    removed imprts in init

--- tableau_utilities/__init__.py
+++ tableau_utilities/__init__.py
@@ -1,5 +1,3 @@
-# from tableau_utilities.scripts.main import do_args
-# from tableau_utilities.scripts.main import main
 from .tableau_file.tableau_file import TableauFileError, Datasource
 from .tableau_file import tableau_file_objects
 from .tableau_server.tableau_server import TableauConnectionError, TableauServer
