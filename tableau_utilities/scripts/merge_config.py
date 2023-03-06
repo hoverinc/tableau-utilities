2319dffb6b52788e0839d966b7e61ecd988e66d6
Jay Rosenthal <jay.rosenthal@panther.io>
2023-03-06 09:49:04 -0800

    Alphabetize datasource list in configs

--- tableau_utilities/scripts/merge_config.py
+++ tableau_utilities/scripts/merge_config.py
@@ -94,6 +94,39 @@ def merge_2_configs(existing_config, additional_config, debugging_logs=False):
     return existing_config
 
 
+def sort_config(config, debugging_logs):
+    """ Takes a config and returns it sorted by key and with the datasources list sorted
+
+    Args:
+        config (dict): A datasource config
+        debugging_logs: Prints info to console if true
+
+    Returns:
+        config (dict): With the list of datasources sorted.  With the config sorted by column_name (top key)
+
+    """
+
+    # Sort the list of datasources alphabetically
+    for k, v in config.items():
+
+        if debugging_logs:
+            print('KEY', k)
+            print('CONGIG', v)
+            print('DATASOURCES', v['datasources'])
+
+        sorted_datasources = sorted(v['datasources'], key=lambda d: d['name'])
+
+        if debugging_logs:
+            print('DATASOURCS SORTED', sorted_datasources)
+
+        config[k]['datasources'] = sorted_datasources
+
+    # Sort the top level keys alphabetically
+    config = dict(sorted(config.items()))
+
+    return config
+
+
 def read_merge_write(existing_config_path, additional_config_path, output_config_path, debugging_logs):
     # Read files
     existing_config = read_file(existing_config_path)
@@ -103,7 +136,7 @@ def read_merge_write(existing_config_path, additional_config_path, output_config
     new_config = merge_2_configs(existing_config, additional_config, debugging_logs)
 
     # Sort and write the merged config
-    new_config = dict(sorted(new_config.items()))
+    new_config = sort_config(new_config, debugging_logs)
     write_file(file_name=output_config_path, config=new_config, debugging_logs=debugging_logs)
 
     print(f'EXISTING CONFIG: {existing_config_path}')
@@ -137,8 +170,8 @@ def merge_configs(args, server=None):
         # Merge
         new_config = add_definitions_mapping(existing_config, definitions_mapping)
         # Sort and write the merged config
-        new_config = dict(sorted(new_config.items()))
-        write_file(file_name=file_name, config=new_config, debugging_logs=args.debugging_logs)
+        new_config = sort_config(new_config, debugging_logs)
+        write_file(file_name=file_name, config=new_config, debugging_logs=debugging_logs)
 
         print(f'EXISTING CONFIG: {existing_config_path}')
         print(f'ADDITIONAL CONFIG: {additional_config_path}')
