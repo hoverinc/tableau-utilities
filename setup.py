5891fb63ee88efcfbca475b8bd37bd96c579a469
Jay Rosenthal <jay.rosenthal@panther.io>
2023-03-06 08:32:19 -0800

    Make 1 requirements file and add requirements to setup

--- setup.py
+++ setup.py
@@ -22,6 +22,12 @@ setup(
     ],
     package_data={'tableau_utilities': ['tableau_file/*.yml']},
     include_package_data=True,
+    install_requires=['xmltodict>=0.12.0',
+                      'pyyaml>=6.0',
+                      'requests>=2.27.1',
+                      'pandas>=1.4.1',
+                      'tabulate>=0.8.9',
+                      'pytest>==7.1.2'],
     entry_points={
         'console_scripts': [
             'tableau_utilities = tableau_utilities.scripts.cli:main',
