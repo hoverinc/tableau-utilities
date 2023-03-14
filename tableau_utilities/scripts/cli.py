bc3870db3fd379dfa3c67bb72f01d5564ff337ca
jaybythebay <jay.rosenthal@panther.io>
2023-03-14 09:09:04 -0700

    Bug Fixes: Color and Download All (#29)
    * Bug Fixes: Color and fix error on id reference in download all

--- tableau_utilities/scripts/cli.py
+++ tableau_utilities/scripts/cli.py
@@ -261,6 +261,7 @@ def tableau_authentication(args):
         'site': args.site_name,
         'server': args.server
     }
+
     if debug:
         for cred_name, cred_value in creds.items():
             if cred_value:
@@ -315,7 +316,9 @@ def tableau_authentication(args):
         host=f'https://{creds["server"]}.online.tableau.com',
         api_version=creds['api_version']
     )
-    color_print(symbol.success, ' Connected to Tableau Server', **title_color)
+    if debug:
+        color_print(symbol.success, ' Connected to Tableau Server', **title_color)
+
     return t
 
 
