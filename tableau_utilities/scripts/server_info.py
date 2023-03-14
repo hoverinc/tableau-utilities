a6f2645362f9ba657ab5d6f0b8f5946fcb7535ca
Justin Grilli <justin.grilli@gmail.com>
2023-03-14 13:12:50 -0700

    Added more print styling to server_infor and server_operate

--- tableau_utilities/scripts/server_info.py
+++ tableau_utilities/scripts/server_info.py
@@ -2,6 +2,7 @@ import pandas as pd
 from tabulate import tabulate
 from pprint import pprint
 
+from tableau_utilities.general.cli_styling import Color, Symbol
 from tableau_utilities.tableau_server.tableau_server import TableauServer
 
 
@@ -15,29 +16,33 @@ def server_info(args, server):
     """
     format = args.list_format
     sort_field = args.list_sort_field
+    # Print Styling
+    color = Color()
+    symbol = Symbol()
 
     if args.list_object:
+        print(f'{color.fg_cyan}{args.list_object.title()}s:{color.reset}')
         # Get server objects, and convert them to dict
         object_list = [o.__dict__ for o in getattr(server, f'get_{args.list_object.lower()}s')()]
         sorted_records = sorted(object_list, key=lambda d: d[sort_field])
         if format == 'names':
             for record in sorted_records:
-                print(record['name'])
+                print(f'  {symbol.arrow_r}', f"{color.fg_yellow}{record['name']}{color.reset}")
         elif format == 'names_ids':
             for record in sorted_records:
-                print(record['name'], record['id'])
+                print(f'  {symbol.arrow_r}', f"{color.fg_yellow}{record['name']} {record['id']}{color.reset}")
         elif format == 'ids_names':
             for record in sorted_records:
-                print(record['id'], record['name'])
+                print(f'  {symbol.arrow_r}', f"{color.fg_yellow}{record['id']} {record['name']}{color.reset}")
         elif format == 'names_projects':
             for record in sorted_records:
-                print(record['name'], record['project_name'])
+                print(f'  {symbol.arrow_r}', f"{color.fg_yellow}{record['name']} {record['project_name']}{color.reset}")
         elif format == 'full_df':
             df = pd.DataFrame(sorted_records)
             print(tabulate(df, headers='keys', tablefmt='psql', colalign='left'))
         elif format == 'full_dictionary':
             for record in sorted_records:
-                print(record)
+                print(f'  {symbol.arrow_r}', f'{color.fg_yellow}{record}{color.reset}')
         elif format == 'full_dictionary_pretty':
             for record in sorted_records:
                 pprint(record)
