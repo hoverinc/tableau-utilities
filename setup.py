ce888d54e4ac900fef180945109cf2f3a603e54a
jaybythebay <jay.rosenthal@hover.to>
2024-05-02 21:51:50 -0700

    bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     long_description=readme,
     long_description_content_type='text/markdown',
     name="tableau_utilities",
-    version="2.1.21",
+    version="2.1.22",
     packages=[
         'tableau_utilities',
         'tableau_utilities.general',
