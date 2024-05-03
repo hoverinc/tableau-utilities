48967ac40aeeb9f147d4e6fb51dff7367695bf4d
jaybythebay <jay.rosenthal@hover.to>
2024-05-02 21:51:10 -0700

    add extras_require

--- setup.py
+++ setup.py
@@ -27,7 +27,8 @@ setup(
                       'requests>=2.27.1,<3.0.0',
                       'pandas>=1.4.1,<2.0.0',
                       'tabulate>=0.8.9,<1.0.0',
-                      'tableauhyperapi==0.0.18825'],
+                      ],
+    extras_require={"HYPER": ['tableauhyperapi==0.0.18825']},
     entry_points={
         'console_scripts': [
             'tableau_utilities = tableau_utilities.scripts.cli:main',
