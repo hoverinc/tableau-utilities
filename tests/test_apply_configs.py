2db4a563c141824d82302a5a87db0a1c3977a00b
Jay Rosenthal <jay.rosenthal@gomotive.com>
2024-08-18 05:54:38 -0700

    prepare configs test

--- tests/test_apply_configs.py
+++ tests/test_apply_configs.py
@@ -128,66 +128,89 @@ def test_flatten_to_list_of_fields(apply_configs):
     assert result == expected_output
 
 
-# def test_prepare_configs(apply_configs):
-#     sample_config_A = {
-#         "Column1": {
-#             "description": "Description of Column1",
-#             "folder": "Folder1",
-#             "persona": "string_dimension",
-#             "datasources": [
-#                 {
-#                     "name": "my_datasource_1",
-#                     "local-name": "MY_COLUMN_1",
-#                     "sql_alias": "MY_COLUMN_1_ALIAS"
-#                 }
-#             ]
-#         },
-#         "Column2": {
-#             "description": "Description of Column2",
-#             "folder": "Folder2",
-#             "persona": "string_dimension",
-#             "datasources": [
-#                 {
-#                     "name": "my_datasource_1",
-#                     "local-name": "MY_COLUMN_1",
-#                     "sql_alias": "MY_COLUMN_1_ALIAS"
-#                 },
-#                 {
-#                     "name": "my_datasource_2",
-#                     "local-name": "MY_COLUMN_2",
-#                     "sql_alias": "MY_COLUMN_2_ALIAS"
-#                 }
-#             ]
-#         }
-#     }
-#
-#     sample_config_B = {
-#         "# ID": {
-#             "description": "Distinct Count of the ID",
-#             "calculation": "COUNTD([ID])",
-#             "folder": "My Data",
-#             "persona": "continuous_number_measure",
-#             "datasources": [
-#                 {
-#                     "name": "my_datasource_1",
-#                     "local-name": "MY_COLUMN_1",
-#                     "sql_alias": "MY_COLUMN_1_ALIAS"
-#                 },
-#                 {
-#                     "name": "my_datasource_2",
-#                     "local-name": "MY_COLUMN_2",
-#                     "sql_alias": "MY_COLUMN_2_ALIAS"
-#                 }
-#             ],
-#             "default_format": "n#,##0;-#,##0"
-#         }
-#     }
-#
-#     expected_output =
-#
-#
-#     result = apply_configs.prepare_configs(sample_config_A, sample_config_B)
-#     assert result == expected_output
+def test_prepare_configs(apply_configs):
+    sample_config_A = {
+        "Column1": {
+            "description": "Description of Column1",
+            "folder": "Folder1",
+            "persona": "string_dimension",
+            "datasources": [
+                {
+                    "name": "my_datasource_1",
+                    "local-name": "MY_COLUMN_1",
+                    "sql_alias": "MY_COLUMN_1_ALIAS"
+                }
+            ]
+        },
+        "Column2": {
+            "description": "Description of Column2",
+            "folder": "Folder2",
+            "persona": "string_dimension",
+            "datasources": [
+                {
+                    "name": "my_datasource_1",
+                    "local-name": "MY_COLUMN_1",
+                    "sql_alias": "MY_COLUMN_1_ALIAS"
+                },
+                {
+                    "name": "my_datasource_2",
+                    "local-name": "MY_COLUMN_2",
+                    "sql_alias": "MY_COLUMN_2_ALIAS"
+                }
+            ]
+        }
+    }
+
+    sample_config_B = {
+        "# ID": {
+            "description": "Distinct Count of the ID",
+            "calculation": "COUNTD([ID])",
+            "folder": "My Data",
+            "persona": "continuous_number_measure",
+            "datasources": [
+                {
+                    "name": "my_datasource_1",
+                    "local-name": "MY_COLUMN_1",
+                    "sql_alias": "MY_COLUMN_1_ALIAS"
+                },
+                {
+                    "name": "my_datasource_2",
+                    "local-name": "MY_COLUMN_2",
+                    "sql_alias": "MY_COLUMN_2_ALIAS"
+                }
+            ],
+            "default_format": "n#,##0;-#,##0"
+        }
+    }
+
+    expected_output = {
+        "Column1": {
+            "description": "Description of Column1",
+            "folder": "Folder1",
+            "persona": "string_dimension",
+            "local-name": "MY_COLUMN_1",
+            "remote_name": "MY_COLUMN_1_ALIAS"
+        },
+        "Column2": {
+            "description": "Description of Column2",
+            "folder": "Folder2",
+            "persona": "string_dimension",
+            "local-name": "MY_COLUMN_1",
+            "remote_name": "MY_COLUMN_1_ALIAS"
+        },
+        "# ID": {
+            "description": "Distinct Count of the ID",
+            "calculation": "COUNTD([ID])",
+            "default_format": "n#,##0;-#,##0",
+            "folder": "My Data",
+            "persona": "continuous_number_measure",
+            "local-name": "MY_COLUMN_1",
+            "remote_name": "MY_COLUMN_1_ALIAS"
+        }
+    }
+
+    result = apply_configs.prepare_configs(sample_config_A, sample_config_B)
+    assert result == expected_output
 
 def test_flatten_to_list_of_fields(apply_configs):
 
@@ -263,6 +286,7 @@ def test_select_matching_datasource_config(apply_configs):
     }
     result = apply_configs.select_matching_datasource_config(sample_config)
     assert result == expected_output
+
 
 if __name__ == '__main__':
     pytest.main()
