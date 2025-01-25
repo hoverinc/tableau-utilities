0cd21449e0c4ea1b975d1c897bbc37bbdb9402f8
Jay Rosenthal <jay.rosenthal@gomotive.com>
2025-01-25 06:11:30 -0800

    bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     long_description=readme,
     long_description_content_type='text/markdown',
     name="tableau_utilities",
-    version="2.2.13",
+    version="2.2.14",
     requires_python=">=3.8",
     packages=[
         'tableau_utilities',
